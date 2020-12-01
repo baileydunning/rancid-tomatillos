e5da8dc17115181f9faaf977305b8aa0e45b3cec
Bruce Gordon <brc.gordon@gmail.com>
2020-12-01 16:22:25 -0700

    Display movie titles on main page

---
+++ src/components/Thumbnail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/Thumbnail.scss';
+
+const Thumbnail = (props) => {
+  return (
+    <p>{ props.title }</p>
+  )
+}
+
+export default Thumbnail
--- src/components/ThumbnailContainer.js
+++ src/components/ThumbnailContainer.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import Thumbnail from './Thumbnail.js'
 import '../css/ThumbnailContainer.scss'
 
-const ThumbnailContainer = (props) => {
-  const thumbnails = this.props.movies.map(movie => {
+const ThumbnailContainer = ({movies}) => {
+  const allMovies = movies.map(movie => {
     return (
       <Thumbnail
         title={movie.title}
+        key={movie.id}
       />
     )
   })
+
+  return (
+    <section className='thumbnail-container'>
+      { allMovies }
+    </section>
+  )
 }
 
 export default ThumbnailContainer
