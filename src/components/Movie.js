2efa27f9affb14d8b4abeb784ccb81c1650c7751
Bailey Dunning <bwdunning@gmail.com>
2020-12-02 22:00:07 -0700

    Add initial CSS styling to movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,12 +3,14 @@ import '../css/Movie.scss'
 
 const Movie = ({ movie }) => {
   return (
-    <section className='movieSection' style={{ background: `url(${ movie.backdrop_path })`}}>
-      <h2>{movie.title}</h2>
-      <article>
-        <img src={ movie.poster_path } alt="movie-poster"></img>
-        <p>{ movie.average_rating.toFixed(1) }</p>
-        <p>{movie.release_date}</p>
+    <section className='movie-section' style={{ background: `url(${ movie.backdrop_path })`}}>
+      <article className="movie-info">
+        <img src={ movie.poster_path } alt="movie-poster" className="movie-poster"></img>
+        <div className="movie-text">
+          <h2>{movie.title}</h2>
+          <p><b>Average Rating:</b> {movie.average_rating.toFixed(1)}</p>
+          <p><b>Release Date:</b> {new Date(movie.release_date).toDateString()}</p>
+        </div>
       </article>
     </section>
   )
