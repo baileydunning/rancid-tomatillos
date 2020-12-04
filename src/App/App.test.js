400521b5bbc0f08aa2a7b03e38e703693bf786a0
Bruce Gordon <brc.gordon@gmail.com>
2020-12-03 18:38:45 -0700

    Initialize App tests

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,6 +1,20 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react'
+import { fireEvent, screen, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Movie from '../Movie/Movie.js'
+import Header from '../Header/Header.js'
+import ThumbnailContainer from '../Thumbnail-Container/ThumbnailContainer.js'
 
+describe('App', () => {
+  beforeEach(() => {
+    render(<App />)
+  })
+  it('should render the App component', () => {
+    const header = screen.getByText('Rancid Tomatillos!')
+    expect(header).toBeInTheDocument()
+  })
+})
 // test('renders learn react link', () => {
 //   render(<App />);
 //   const linkElement = screen.getByText(/learn react/i);
