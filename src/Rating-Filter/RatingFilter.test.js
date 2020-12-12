b43d1a7baa6a370cdb3ed830acf1727787573453
Bruce Gordon <brc.gordon@gmail.com>
2020-12-12 15:17:46 -0700

    Test RatingFilter component

---
+++ src/Rating-Filter/RatingFilter.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import RatingFilter from './RatingFilter.js';
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-Event';
+
+describe('RatingFilter', () => {
+  const mockUpdateRating = jest.fn();
+
+  beforeEach(() => {
+    render(
+      <RatingFilter
+      updateRating={ mockUpdateRating }
+      />
+    )
+  })
+
+  it('should render min and max inputs on the home page', () => {
+    const min = screen.getByPlaceholderText("min");
+    const max = screen.getByPlaceholderText("max");
+    expect(min).toBeInTheDocument();
+    expect(max).toBeInTheDocument();
+  })
+
+  it('should call updateText on change', () => {
+    const min = screen.getByPlaceholderText("min");
+    userEvent.type(min, "7")
+    expect(mockUpdateRating).toHaveBeenCalled()
+    expect(min.value).toBe("7")
+  })
+})
